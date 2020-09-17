b1e42cb1f3ca90ce6ecaf9ea97b6bf091d046c30
DanielFerreira <danielfferreirabr@gmail.com>
2020-09-17 04:37:47 -0300

    Adjusting device dimensions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const $header = document.querySelector('header');
-const $logo = document.querySelectorAll('.grid-4')[0];
-window.addEventListener('scroll',toggleHeader,false);
-function toggleHeader() {
-  if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
-    $header.classList.remove('max-header');
-    $header.classList.add('min-header');
-    $logo.classList.remove('max-grid-4');
-    $logo.classList.add('min-grid-4');
-  }
-  else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
-    $header.classList.add('max-header');
-    $header.classList.remove('min-header');
-    $logo.classList.add('max-grid-4');
-    $logo.classList.remove('min-grid-4');
-  }
-}
+// const $header = document.querySelector('header');
+// const $logo = document.querySelectorAll('.grid-4')[0];
+// window.addEventListener('scroll',toggleHeader,false);
+// function toggleHeader() {
+//   if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
+//     $header.classList.remove('max-header');
+//     $header.classList.add('min-header');
+//     $logo.classList.remove('max-grid-4');
+//     $logo.classList.add('min-grid-4');
+//   }
+//   else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
+//     $header.classList.add('max-header');
+//     $header.classList.remove('min-header');
+//     $logo.classList.add('max-grid-4');
+//     $logo.classList.remove('min-grid-4');
+//   }
+// }
