c100cc31e9071ad04ef00d6f66dd66da36b472ff
DanielFerreira <danielfferreirabr@gmail.com>
2020-09-22 19:37:48 -0300

    Finished project

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-// const $header = document.querySelector('header');
-// const $logo = document.querySelectorAll('.grid-4')[0];
-// window.addEventListener('scroll',toggleHeader,false);
-// function toggleHeader() {
-//   if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
-//     $header.classList.remove('max-header');
-//     $header.classList.add('min-header');
-//     $logo.classList.remove('max-grid-4');
-//     $logo.classList.add('min-grid-4');
-//   }
-//   else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
-//     $header.classList.add('max-header');
-//     $header.classList.remove('min-header');
-//     $logo.classList.add('max-grid-4');
-//     $logo.classList.remove('min-grid-4');
-//   }
-// }
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+}
