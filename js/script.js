d30623b56d213fdaf4793178815844ac5fc6e2e1
DanielFerreira <danielfferreirabr@gmail.com>
2020-09-22 19:48:50 -0300

    Adjusting the page interactions script file

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ if (window.SimpleSlide) {
 
 	new SimpleSlide({
 		slide: "quote", // nome do atributo data-slide="principal"
-		time: 5000 // tempo de transição dos slides
+		time: 3000 // tempo de transição dos slides
 	});
 
 	new SimpleSlide({
 		slide: "portfolio",
-		time: 5000,
+		time: 3000,
 		nav: true
 	});
 
