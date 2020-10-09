c59c39845cc3a96615ad991197a006738e8c6a29
DanielFerreira <danielfferreirabr@gmail.com>
2020-10-09 11:01:05 -0300

    script and finished project

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ if (window.SimpleSlide) {
 
 	new SimpleSlide({
 		slide: "quote", // nome do atributo data-slide="principal"
-		time: 3000 // tempo de transição dos slides
+		time: 5000 // tempo de transição dos slides
 	});
 
 	new SimpleSlide({
 		slide: "portfolio",
-		time: 3000,
+		time: 5000,
 		nav: true
 	});
 
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
 }
